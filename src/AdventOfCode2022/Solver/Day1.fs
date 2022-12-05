88ad50103d1e190935b2d08c7a614bf897ddd057
Martin Thaller <thaller.martin@gmail.com>
2022-12-05 12:10:48 +0100

    day 1

--- src/AdventOfCode2022/Solver/Day1.fs
+++ src/AdventOfCode2022/Solver/Day1.fs
@@ -1,21 +1,20 @@
 ﻿module AdventOfCode2022.Solver.Day1
 
-let rec countIncreasing numbers acc =
-    match numbers with
-    | [] -> acc
-    | [ _ ] -> acc
-    | head :: tail -> countIncreasing tail (if head < tail.[0] then acc + 1 else acc)
+let sumInventories =
+    Array.fold
+        (fun (state: List<int>) el ->
+            match el with
+            | "" -> 0 :: state
+            | num -> (state.Head + (num |> int)) :: state.Tail)
+        (0 :: List.empty)
 
 let solver1 (lines: string array) =
-    let ints = lines |> Array.map int |> Array.toList
-    countIncreasing ints 0 |> string
+    lines |> sumInventories |> List.max |> string
 
 let solver2 (lines: string array) =
-    let ints = lines |> Array.map int |> Array.toList
-
-    let rec getWindowedNumbers (numbers: int list) windowed =
-        match numbers with
-        | n1 :: n2 :: n3 :: tail -> getWindowedNumbers (n2 :: n3 :: tail) ((n1 + n2 + n3) :: windowed)
-        | _ -> windowed
-
-    countIncreasing (getWindowedNumbers ints [] |> List.rev) 0 |> string
+    lines
+    |> sumInventories
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
+    |> string
--- test/AdventOfCode2022.Tests/Day1Tests.fs
+++ test/AdventOfCode2022.Tests/Day1Tests.fs
@@ -5,23 +5,28 @@ open Xunit
 
 type Day1Tests() =
     let demoData =
-        [| "199"
-           "200"
-           "208"
-           "210"
-           "200"
-           "207"
-           "240"
-           "269"
-           "260"
-           "263" |]
+        [| "1000"
+           "2000"
+           "3000"
+           ""
+           "4000"
+           ""
+           "5000"
+           "6000"
+           ""
+           "7000"
+           "8000"
+           "9000"
+           ""
+           "10000"
+           |]
 
     [<Fact>]
     let ``Day 1 part 1`` () =
         let solution = Day1.solver1 demoData
-        Assert.Equal("7", solution)
+        Assert.Equal("24000", solution)
 
     [<Fact>]
     let ``Day 1 part 2`` () =
         let solution = Day1.solver2 demoData
-        Assert.Equal("5", solution)
+        Assert.Equal("45000", solution)
