cc70c2973b1377ed08a28fb394b7b562cb9676d7
Martin Thaller <thaller.martin@gmail.com>
2022-12-05 12:10:49 +0100

    Day 4

---
+++ src/AdventOfCode2022/Solver/Day4.fs
@@ -0,0 +1,28 @@
+﻿module AdventOfCode2022.Solver.Day4
+
+
+let getRange(assignment: string) =
+    let split = assignment.Split '-'
+    let start = split.[0] |> int
+    let ending = split.[1] |> int
+    seq { for i in start .. ending -> i } |> Set.ofSeq
+
+let doRangesOverlapCompletely(line: string) =  
+    let split = line.Split ','
+    let a = split.[0] |> getRange
+    let b = split.[1] |> getRange
+    let intersection = Set.intersect a b
+    intersection.Count = a.Count || intersection.Count = b.Count
+
+let doRangesOverlapPartly(line: string) =  
+    let split = line.Split ','
+    let a = split.[0] |> getRange
+    let b = split.[1] |> getRange
+    let intersection = Set.intersect a b
+    intersection.Count > 0
+    
+let solver1 (lines: string array) =
+    lines |> Array.map doRangesOverlapCompletely |> Array.filter id |> Array.length
+
+let solver2 (lines: string array) =
+    lines |> Array.map doRangesOverlapPartly |> Array.filter id |> Array.length
---
+++ test/AdventOfCode2022.Tests/Day4Tests.fs
@@ -0,0 +1,26 @@
+﻿module AdventOfCode2022.Tests.Day4Tests
+
+open AdventOfCode2022.Solver
+open Xunit
+
+type Day4Test() =
+    let demoData =
+        [| 
+            "2-4,6-8"
+            "2-3,4-5"
+            "5-7,7-9"
+            "2-8,3-7"
+            "6-6,4-6"
+            "2-6,4-8"
+            |]
+
+    [<Fact>]
+    let ``Day 4 part 1`` () =
+        let solution = Day4.solver1 demoData
+        Assert.Equal(2, solution)
+
+
+    [<Fact>]
+    let ``Day 4 part 2`` () =
+        let solution = Day4.solver2 demoData
+        Assert.Equal(4, solution)
