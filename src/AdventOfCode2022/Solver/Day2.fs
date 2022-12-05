0a1a8d0e1da2e31334f27db0f6c4bce0511f97f1
Martin Thaller <thaller.martin@gmail.com>
2022-12-05 12:10:49 +0100

    day 2

---
+++ src/AdventOfCode2022/Solver/Day2.fs
@@ -0,0 +1,57 @@
+﻿module AdventOfCode2022.Solver.Day2
+
+let getSignValue1 (line: string) =
+    let c = line.[2]
+    match c with
+    | 'X' -> 1
+    | 'Y' -> 2
+    | 'Z' -> 3
+    | _ -> failwith "sign does not exist"
+
+let getGameResult1 (line: string) =
+    let a = line.[0]
+    let b = line.[2]
+    match a, b with
+    | 'A', 'X'
+    | 'B', 'Y'
+    | 'C', 'Z' -> 3
+    | 'A', 'Y'
+    | 'B', 'Z'
+    | 'C', 'X' -> 6
+    | 'A', 'Z'
+    | 'B', 'X'
+    | 'C', 'Y' -> 0
+    | _ -> failwith "combination does not exist"
+
+let getSignValue2 (line: string) =
+    let a = line.[0]
+    let b = line.[2]
+    match a, b with
+    | 'A', 'Y'
+    | 'B', 'X'
+    | 'C', 'Z' -> 1
+    | 'B', 'Y'
+    | 'A', 'Z'
+    | 'C', 'X' -> 2
+    | 'B', 'Z'
+    | 'A', 'X'
+    | 'C', 'Y' -> 3
+    | _ -> failwith "combination does not exist"
+
+let getGameResult2 (line: string) =
+    let c = line.[2]
+    match c with
+    | 'X' -> 0
+    | 'Y' -> 3
+    | 'Z' -> 6
+    | _ -> failwith "result does not exist"
+
+let solver1 (lines: string array) =
+    lines
+    |> Array.map (fun el -> (getSignValue1 el) + (getGameResult1 el))
+    |> Array.sum
+
+let solver2 (lines: string array) =
+    lines
+    |> Array.map (fun el -> (getSignValue2 el) + (getGameResult2 el))
+    |> Array.sum
---
+++ test/AdventOfCode2022.Tests/Day2Tests.fs
@@ -0,0 +1,18 @@
+﻿module AdventOfCode2022.Tests.Day2Tests
+
+open AdventOfCode2022.Solver
+open Xunit
+
+type Day2Test() =
+    let demoData = [| "A Y"; "B X"; "C Z" |]
+
+    [<Fact>]
+    let ``Day 2 part 1`` () =
+        let solution = Day2.solver1 demoData
+        Assert.Equal(15, solution)
+
+
+    [<Fact>]
+    let ``Day 2 part 2`` () =
+        let solution = Day2.solver2 demoData
+        Assert.Equal(12, solution)
