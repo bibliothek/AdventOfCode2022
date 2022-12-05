3d405768407254853c60edd5ec97577c624d0386
Martin Thaller <thaller.martin@gmail.com>
2022-12-05 12:10:49 +0100

    Day 3

---
+++ src/AdventOfCode2022/Solver/Day3.fs
@@ -0,0 +1,42 @@
+﻿module AdventOfCode2022.Solver.Day3
+
+let findCommonItem (line: string) =
+    let halfPoint = line.Length / 2
+
+    let compartment1 =
+        line.Substring(0, halfPoint).ToCharArray()
+        |> Set.ofArray
+
+    let compartment2 =
+        line.Substring(halfPoint).ToCharArray()
+        |> Set.ofArray
+
+    let common =
+        Set.intersect compartment1 compartment2
+
+    common.MinimumElement
+
+let getItemValue (item: char) =
+    let i = int item
+    if i > 96 then i - 96 else (i - 64) + 26
+
+
+let findCommonBadge (groupInventories: string array) =
+    let common =
+        groupInventories
+        |> Array.map (fun x -> x.ToCharArray() |> Set.ofArray)
+        |> Set.intersectMany
+
+    common.MinimumElement
+
+
+let solver1 (lines: string array) =
+    lines
+    |> Array.map (findCommonItem >> getItemValue)
+    |> Array.sum
+
+let solver2 (lines: string array) =
+    lines
+    |> Array.chunkBySize 3
+    |> Array.map (findCommonBadge >> getItemValue)
+    |> Array.sum
---
+++ test/AdventOfCode2022.Tests/Day3Tests.fs
@@ -0,0 +1,26 @@
+﻿module AdventOfCode2022.Tests.Day3Tests
+
+open AdventOfCode2022.Solver
+open Xunit
+
+type Day3Test() =
+    let demoData =
+        [|
+            "vJrwpWtwJgWrhcsFMMfFFhFp"
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+            "PmmdzqPrVvPwwTWBwg"
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+            "ttgJtRGJQctTZtZT"
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        |]
+
+    [<Fact>]
+    let ``Day 3 part 1`` () =
+        let solution = Day3.solver1 demoData
+        Assert.Equal(157, solution)
+
+
+    [<Fact>]
+    let ``Day 3 part 2`` () =
+        let solution = Day3.solver2 demoData
+        Assert.Equal(70, solution)
